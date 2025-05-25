ae3a31959ae8468fb407610c9b85387e45b61cc9
RPi4 <snkpost@yandex.ru>
2025-05-26 02:00:18 +0300

    Add secret.h

---
+++ src/secret.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define SSID "YourWiFi"
+#define PASSWORD "YourPassword"
+#define BOT_TOKEN "YourTelegramBotToken"              // Telegram BOT Token (Get from Botfather)
+#define LATITUDE latitude_of_your_weather_station     // For example 51.500833
+#define LONGITUDE longitude_of_your_weather_station   // For example -0.124444
